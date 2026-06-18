5cfb71549cfe74fcc992734ecdc750c60b9908d2
Chukwuka Uffoh <cuffoh@infinion.co>
2026-06-18 15:12:19 +0100

    Made fresh frontend push

--- security.js
+++ security.js
@@ -396,7 +396,7 @@
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob:",
       "media-src 'self' data:",
-      "connect-src 'self' http://127.0.0.1:3002 https://uffoh-portfolio-backend.azurewebsites.net",
+      "connect-src 'self' http://127.0.0.1:3002 https://uffoh-portfolio-backend-czexhae2c8dqhvg0.uksouth-01.azurewebsites.net",
       "frame-ancestors 'none'",                      // reinforces clickjacking guard
       "form-action 'self'",
       "base-uri 'self'",
